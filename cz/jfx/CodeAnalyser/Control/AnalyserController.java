24e56730e9be4db81d92c18d7d19bbc4e7a5bc36
Milan Sulc <felix@noblexity.com>
2012-04-19 16:20:17 +0200

    Added CodeFilter

--- cz/jfx/CodeAnalyser/Control/AnalyserController.java
+++ cz/jfx/CodeAnalyser/Control/AnalyserController.java
@@ -1,7 +1,10 @@
 package cz.jfx.CodeAnalyser.Control;
 
+import cz.jfx.CodeAnalyser.Filters.CodeFilter;
 import cz.jfx.CodeAnalyser.Storage.IStorage;
 import cz.jfx.CodeAnalyser.TaskManager.TaskManager;
+import java.io.File;
+import java.io.FileFilter;
 
 /**
  *
@@ -10,25 +13,26 @@ import cz.jfx.CodeAnalyser.TaskManager.TaskManager;
 public class AnalyserController {
 
     private TaskManager taskManager;
-    private IStorage fileStorage;
-    private IStorage folderStorage;
+    private IStorage<File> fileStorage;
+    private IStorage<File> folderStorage;
+    private FileFilter codeFilter = new CodeFilter();
 
     private AnalyserController() {
     }
 
-    public IStorage getFileStorage() {
+    public IStorage<File> getFileStorage() {
         return fileStorage;
     }
 
-    public void setFileStorage(IStorage fileStorage) {
+    public void setFileStorage(IStorage<File> fileStorage) {
         this.fileStorage = fileStorage;
     }
 
-    public IStorage getFolderStorage() {
+    public IStorage<File> getFolderStorage() {
         return folderStorage;
     }
 
-    public void setFolderStorage(IStorage folderStorage) {
+    public void setFolderStorage(IStorage<File> folderStorage) {
         this.folderStorage = folderStorage;
     }
 
@@ -42,6 +46,10 @@ public class AnalyserController {
 
     public static AnalyserController getInstance() {
         return AnalyserControllerHolder.INSTANCE;
+    }
+
+    public FileFilter getCodeFilter() {
+        return codeFilter;
     }
 
     private static class AnalyserControllerHolder {
