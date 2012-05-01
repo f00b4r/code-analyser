d8ba7386402bf0a77d56cee67ab080f743fe5cbd
Milan Sulc <felix@noblexity.com>
2012-05-01 17:13:54 +0200

    Pridano defaultni nastaveni a settery/gettery

--- src/cz/jfx/CodeAnalyser/Config/Config.java
+++ src/cz/jfx/CodeAnalyser/Config/Config.java
@@ -1,12 +1,14 @@
 package cz.jfx.CodeAnalyser.Config;
 
 import cz.jfx.CodeAnalyser.Control.AnalyserController;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 
 /**
  *
@@ -14,32 +16,75 @@ import java.util.ResourceBundle;
  */
 public class Config {
 
-    public static final Config instance = new Config();
+    private static final Config instance = new Config();
     private ResourceBundle application = ResourceBundle.getBundle("cz/jfx/CodeAnalyser/Config/application");
     private ResourceBundle defaults = ResourceBundle.getBundle("cz/jfx/CodeAnalyser/Config/defaults");
     private Properties properties = new Properties();
 
+    /**
+     * Gets a application config value
+     * @param key
+     * @return 
+     */
     public static String getValue(String key) {
         if (instance.application.containsKey(key)) {
             return instance.application.getString(key);
         }
+        AnalyserController.logger.log(Level.WARNING, "Key {0} not found in bundle", key);
         return null;
     }
 
+    /**
+     * Gets a custom(defaul) setting value
+     * @param key
+     * @return 
+     */
+    public static String getProperty(String key) {
+        if (instance.properties.containsKey(key)) {
+            return instance.properties.getProperty(key);
+        }
+        AnalyserController.logger.log(Level.WARNING, "Key {0} not found in properties", key);
+        return null;
+    }
+    
+    /**
+     * Sets a Object o to String key
+     * @param key
+     * @param o 
+     */
+    public static void saveProperty(String key, Object o) {
+        instance.properties.put(key, o);
+    }
+
+    /**
+     * Loads config from properly file
+     * @param filename 
+     */
     public void load(String filename) {
         try {
-            properties.load(new FileInputStream(filename));
+            // Check if config file exists
+            File configFile = new File(filename);
+            if (configFile.exists()) {
+                // if yes, load custom settings
+                properties.load(new FileInputStream(filename));
+            } else {
+                // if not, store default settings
+                storeDefaults(defaults);
+                store(filename);
+            }
+
         } catch (IOException e) {
             AnalyserController.logger.warning("load fail");
             AnalyserController.logger.warning(e.getMessage());
         }
     }
 
+    /**
+     * Write config to properly file
+     * @param filename 
+     */
     public void store(String filename) {
         try {
-            if (properties.isEmpty()) {
-                storeDefaults(defaults);
-            }
             properties.store(new FileOutputStream(filename), "CodeAnalyse: default settings");
         } catch (IOException e) {
             AnalyserController.logger.warning("store fail");
@@ -47,6 +92,10 @@ public class Config {
         }
     }
 
+    /**
+     * Fill first defaults data
+     * @param resource 
+     */
     private void storeDefaults(ResourceBundle resource) {
         Enumeration<String> keys = resource.getKeys();
         while (keys.hasMoreElements()) {
@@ -55,6 +104,10 @@ public class Config {
         }
     }
 
+    /**
+     * Return Config instance
+     * @return 
+     */
     public static Config getInstance() {
         return instance;
     }
